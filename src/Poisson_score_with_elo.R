25185d53338725f82e5d79b50876c8fa58c32ea1
Npaffen <nils.paffen@wopic.de>
2020-04-22 12:46:28 +0200

    push

--- src/Poisson_score_with_elo.R
+++ src/Poisson_score_with_elo.R
@@ -2,10 +2,11 @@ library(readr)
 library(tidyverse)
 database_mr <- read_rds(str_c(here::here() , "data", "database_match_results_1920.rds", sep = "/")) 
 
+missinggames <- read_rds(here::here("/data/database_missing_matches_1920.rds"))
 
 database_season <- readRDS(here::here("/data/database_season.rds"))  
 
-source(here::here("src/functions.R"))
+source(here::here("src/functions_N.R"))
 #Idee für Untenschieden : Ausrechen wie häufig unentschieden in dieser Saison gespielt wurde. 
 #Annahme : Zwei gleichstarke Mannschaften haben eine höhere Wahrscheinlichkeit unentschieden zu spielen als zwei unterschiedlich Starke.
 #Ableitung : Für gleichstarke Mannschaften Durchschnittswert für unterschiedlich Starke den Wert diskontieren 
@@ -33,6 +34,13 @@ poisson_model <-
   glm(goals ~ home + team +opponent, family=poisson(link=log),data=.)
 summary(poisson_model)
 
+matchday30 <- databse_season %>% filter(season == 1920, matchday == 30)
 
 
-simulate_match(poisson_model, "Chelsea", "Sunderland", max_goals=4)
+N <- 5000 # simulation runs
+sim_output <- f_score_prob_matches(missinggames, matchday30, poisson_model,max_goals = 10, N, limit = 0.01)
+all_final_tables <- sim_output[[1]]
+sim_output[[2]] # print convergence plot
+average_table <- aggregate(all_final_tables[,-1],
+                           by = list(all_final_tables$club_name),
+                           FUN = "mean")
