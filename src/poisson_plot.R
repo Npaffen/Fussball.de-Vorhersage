5bc93e107eeb43ffcb5528b3f766a7a81bb9e3c1
Npaffen <nils.paffen@wopic.de>
2020-10-31 23:13:03 +0100

    Poisson

--- src/poisson_plot.R
+++ src/poisson_plot.R
@@ -39,34 +39,32 @@ poisson_plot<- function(sim_years){
   summary(poisson_model)
   
   
-  goals<- f_simulate_score_prob_poisson(foot_model = poisson_model,
+ sim <- f_simulate_score_prob_poisson(foot_model = poisson_model,
                                         homeTeam = missinggames$club_name_home,
                                         awayTeam = missinggames$club_name_away
-  )
-  actual <- read_rds(here::here(paste0("/data/database_match_results_",sim_years,".rds")))
-
-  png(here::here(paste0("paper/plots/poisson_actual_home_",sim_years,".png")))
- home <- tibble(poisson_away = goals$home,
-         actual_away = actual$goals_team_home
-  )%>% gather(key=Type, value=Value) %>% 
-    ggplot(aes(x=Value,fill=Type)) + 
-    geom_histogram(position="dodge")+
-    labs(x = 'Goals per Match' )+
-   labs(x = 'Goals per Match', y = 'Frequency of Goals' )
- home + scale_x_continuous(breaks = 0:15)+
-   scale_y_continuous(breaks = seq(0,100,5))
- dev.off()
- 
- png(here::here(paste0("paper/plots/poisson_actual_away_",sim_years,".png")))
- away <- tibble(poisson_away = goals$away,
-         actual_away = actual$goals_team_away
-  )%>% gather(key=Type, value=Value) %>% 
-    ggplot(aes(x=Value,fill=Type)) + 
-    geom_histogram(position="dodge")+
-    labs(x = 'Goals per Match', y = 'Frequency of Goals' )
-away + scale_x_continuous(breaks = 0:10)+
-  scale_y_continuous(breaks = seq(0,100,5))
-dev.off()
+  ) 
+poisson <- tibble(all_goals = c(sim$home, sim$away))  
+return(poisson)
  }
 sim_years <- c(1617,1718,1819)
-map(sim_years, ~poisson_plot(.x))
+poisson_method <- map_df(sim_years, ~poisson_plot(.x)) 
+
+
+
+actual <- rbind(read_rds(here::here(paste0("/data/database_match_results_",sim_years[1],".rds"))) %>% .[,5:6],
+                read_rds(here::here(paste0("/data/database_match_results_",sim_years[2],".rds"))) %>% .[,5:6],
+                read_rds(here::here(paste0("/data/database_match_results_",sim_years[3],".rds"))) %>% .[,5:6])
+
+actual <- tibble(all_goals = c(actual$goals_team_home, actual$goals_team_away))
+png(file = here::here(paste0("paper/plots/poisson_actual_home.png")) )
+poisson <- tibble(poisson = poisson_method$all_goals,
+               actual = actual$all_goals
+)%>% gather(key=Type, value=Value) %>% 
+  ggplot(aes(x=Value,fill=Type)) + 
+  geom_histogram(position="dodge")+
+  labs(x = 'Goals per Match' )+
+  labs(x = 'Goals per Match', y = 'Frequency of Goals' )+ 
+  scale_x_continuous(breaks = 0:20)+
+  scale_y_continuous(breaks = seq(0,800,50))
+print(poisson)
+dev.off()
